94ad49095905518f8578c5111ec83d0558dfecfa
shirai0323 <a.shirai0323@gmail.com>
2023-05-06 22:46:19 +0900

    [Add]テストデータ作成

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,8 +10,11 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
-    public function run(): void
+    public function run()
     {
+        {
+            $this->call(UsersTableSeeder::class);
+        }
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
