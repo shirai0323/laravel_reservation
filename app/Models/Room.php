14f3e35f8fa43d04863c5814188f7798cf45380c
shirai0323 <a.shirai0323@gmail.com>
2023-05-08 20:42:36 +0900

    [Add]モデルに時間取得追加

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Room extends Model
 {
     use HasFactory;
+
+    protected $appends = ['time_step_values'];
+
+    public function getTimeStepValuesAttribute() { //１時間の間に予約できる「分」を取得する
+        $time_step_values = [];
+        $count = 60 / $this->time_steps;
+
+        for($i = 0 ; $i < $count ; $i++) {
+
+            $time_step_values[] = $this->time_steps * $i;
+
+        }
+
+        return $time_step_values;
+    }
 }
